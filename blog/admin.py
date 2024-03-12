213791f4d228212aba46760757b1e1c063a464e3
MuhammedUyan <muhammeduyan4624@gmail.com>
2024-03-12 05:38:05 +0300

    kullanıcı girişi olan kısma kadar yapıldı

---
+++ blog/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Blog, Category
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active', 'is_home', 'slug', 'category')
+    list_editable = ('is_active', 'is_home')
+    search_fields = ('title', 'description')
+    readonly_fields = ('slug',)
+    list_filter = ('is_active', 'is_home', 'category')
+
+    def category(self, obj):
+        html = ""
+
+        for category in obj.categories.all():
+            html += category.name + "<br>"
+        return html
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    readonly_fields = ('slug',)
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+# http://localhost:8000/          => homepage
+# http://localhost:8000/index     => homepage
+# http://localhost:8000/blogs     => blogs
+# http://localhost:8000/blogs/3   => blogs-details
+
+
+urlpatterns = [
+    path("", views.index, name="home"),
+    path("index/", views.index),
+    path("blogs/", views.blogs, name="blogs"),
+    path("category/<slug:slug>", views.blogs_by_category, name="blogs_by_category"),
+    path("blogs/<slug:slug>", views.blog_details, name="blog_details"),
+]
