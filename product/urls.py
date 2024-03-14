2c23fb1555e671e646f701dba38db8b1edc35a10
MuhammedUyan <muhammeduyan4624@gmail.com>
2024-03-15 01:22:53 +0300

    anasayfaya slider eklendi ve footer düzenlendi

--- product/urls.py
+++ product/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("index/", views.index),
+    path("index/", views.index,),
     path("product_category/<slug:slug>", views.products_by_category, name="products_by_category"),
     path('products/', views.product, name='products'),
 ]
